ee6f675a3adbfcac03bb2f2b209288384d4ced7a
milko <ph.watcharapol_st@tni.ac.th>
2024-02-12 15:05:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tni2013111089/Week5.java
@@ -0,0 +1,14 @@
+package com.example.tni2013111089;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Week5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week5);
+    }
+}
